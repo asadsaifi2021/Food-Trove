c4d00286001e836f5c5c43086c007519ff8c724f
Asad Saifi <89274067+asadsaifi2021@users.noreply.github.com>
2023-03-28 15:00:20 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,18 @@
-// category buttons
-function handleButtonClick(category, button) {
-  //unhide the list
-  hideItems.classList.remove("hidden");
+// // category buttons
+// function handleButtonClick(category, button) {
+//   //unhide the list
+//   hideItems.classList.remove("hidden");
 
-  // un-select every button
-  const buttons = document.querySelectorAll(".category-button");
-  buttons.forEach((button) => button.classList.remove("selected"));
+//   // un-select every button
+//   const buttons = document.querySelectorAll(".category-button");
+//   buttons.forEach((button) => button.classList.remove("selected"));
 
-  const landing = document.querySelector(".landing");
-  landing.classList.add("hidden");
+//   const landing = document.querySelector(".landing");
+//   landing.classList.add("hidden");
 
-  // select the current button
-  button.classList.add("selected");
-
-  // if the category is selected
-  if (category === "grains") {
-    console.log("Grains!");
-  } else if (category == "fruits") {
-    console.log("Fruits!");
-  } else if (category == "vegetables") {
-    console.log("Vegetables!");
-  } else if (category == "protein") {
-    console.log("Protein!");
-  } else if (category == "dairy") {
-    console.log("Dairy!");
-  } else {
-    console.log("This should not happen");
-  }
-}
+//   // select the current button
+//   button.classList.add("selected");
+// }
 
 // define
 function buttonHover(button) {
