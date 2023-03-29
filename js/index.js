89026b04945a0df26315c79d783b92ff231bf489
Asad Saifi <89274067+asadsaifi2021@users.noreply.github.com>
2023-03-28 20:06:07 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-// // category buttons
-// function handleButtonClick(category, button) {
-//   //unhide the list
-//   hideItems.classList.remove("hidden");
-
-//   // un-select every button
-//   const buttons = document.querySelectorAll(".category-button");
-//   buttons.forEach((button) => button.classList.remove("selected"));
-
-//   const landing = document.querySelector(".landing");
-//   landing.classList.add("hidden");
-
-//   // select the current button
-//   button.classList.add("selected");
-// }
-
 // define
 function buttonHover(button) {
   button.classList.add("hovered");
